03c1296d1bdbe94208984e82d81d88a8c47a262f
Okan Inceoglu <twovoss@gmail.com>
2026-05-02 23:33:51 +0300

    added mergeSort to knowledge

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,6 +1,56 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
+//-------------------------------------------------------------------------------------------------
+void merge(int arr[], int l, int m, int r) {
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int L[100], R[100];
+
+	for (int i = 0;i < n1;i++) {
+		L[i] = arr[l + i];
+	}
+	for (int j = 0;j < n2;j++) {
+		R[j] = arr[r + j + 1];
+	}
+
+	int i = 0, j = 0, k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+}
+void mergeSort(int arr[], int l, int r) {
+	if (l >= r)return;
+	int m = (l + r) / 2;
+	mergeSort(arr, l, m);
+	mergeSort(arr, m + 1, r);
+
+	merge(arr, l, m, r);
+}
+//-------------------------------------------------------------------------------------------------
+
+
 bool PrimeNumber(int n) {
 	if (n <= 1)return false;
 	if (n == 2)return true;
