c85a17f1b43eb88d9eda710dca4b18126deb6366
Okan Inceoglu <twovoss@gmail.com>
2026-05-09 21:21:33 +0300

    + QuickSort and ShellSort(Upgrading type of InsertionSort)

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,6 +1,45 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
+
+void shellSort(int arr[], int n) {
+	for (int gap = n / 2;gap > 0;gap /= 2) {
+		for (int i = gap;i < n;i++) {
+			int key = arr[i];
+			int j = i;
+			while (j >= gap && arr[j - gap] > key) {
+				arr[j] = arr[j - gap];
+				j -= gap;
+			}
+			arr[j] = key;
+		}
+	}
+}
+//-------------------------------------------------------------------------------------------------
+int partition(int arr[], int l, int r) {
+	int pivot = arr[r];
+	int i = l - 1;
+
+	for (int j = l;j < r;j++) {
+		if (arr[j] < pivot) {
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[r]);
+	return i + 1;
+}
+
+void quickSort(int arr[], int l, int r) {
+	if (l < r) {
+		int pi = partition(arr, l, r);
+
+		quickSort(arr, l, pi - 1);
+		quickSort(arr, pi + 1, r);
+	}
+
+}
+
 //-------------------------------------------------------------------------------------------------
 void merge(int arr[], int l, int m, int r) {
 	int n1 = m - l + 1;
